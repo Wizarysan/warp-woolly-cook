d7fa8ff1721f3bd8825205bc6826106155cd29d7
Glitch (glitch-hello-react) <none>
2021-06-11 07:47:45 +0000

    🍥🌛 Checkpoint ./src/pages/home.jsx:4491163/583

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,20 +5,27 @@ import { Link } from "wouter";
 
 export default function Home() {
   const [todos, setTodos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const onChange = (e) => setSearchTerm(e.target.value);
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then(response => response.json())
       .then(json => setTodos(json))
   }, []);
+  
+  const matchingTodos = searchTerm ? todos.filter((todo) => todo.title.startsWith(searchTerm)) : todos;
+
   return (
     <>
       <h1> TODO list </h1>
+      <input type="text" value={searchTerm} onChange={onChange} />
       <div style={{
         display: 'flex',
         flexWrap: 'wrap',
       }}>
       {
-        todos.map((todo) => (
+        matchingTodos.map((todo) => (
           <div style={{
             border: 'solid 1px',
             padding: 20,
