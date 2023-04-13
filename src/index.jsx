03465c3866f22a51ef673867543d0280b160f45d
Glitch (glitch-hello-react) <none>
2023-04-13 21:04:09 +0000

    🍃💝 Checkpoint ./src/index.jsx:57019104/77 ./package.json:57019104/1373

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./app.jsx";
 import { HelmetProvider } from 'react-helmet-async';
 
